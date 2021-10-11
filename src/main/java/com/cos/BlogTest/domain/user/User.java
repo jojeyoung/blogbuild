fdc67ee1c3ba6498c432fa5f9cc103765b4b2c19
jojeyoung <976400@naver.com>
2021-10-11 14:55:48 +0900

    회원수정

--- src/main/java/com/cos/BlogTest/domain/user/User.java
+++ src/main/java/com/cos/BlogTest/domain/user/User.java
@@ -10,12 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
+@Setter
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/cos/BlogTest/web/UserController.java
+++ src/main/java/com/cos/BlogTest/web/UserController.java
@@ -1,17 +1,29 @@
 package com.cos.BlogTest.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.BlogTest.domain.user.User;
 import com.cos.BlogTest.domain.user.UserRepository;
+import com.cos.BlogTest.handler.ex.MyAsyncNotFoundException;
 import com.cos.BlogTest.utill.SHA;
+import com.cos.BlogTest.web.dto.CMRespDto;
 import com.cos.BlogTest.web.dto.JoinReqDto;
 import com.cos.BlogTest.web.dto.LoginReqDto;
+import com.cos.BlogTest.web.dto.UserUpdateDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,8 +32,38 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 	
 	private final UserRepository userRepository;
-	private final HttpSession Session;
+	private final HttpSession session;
 	
+	
+	@PutMapping("/user/{id}")
+	public @ResponseBody CMRespDto<String> update(@PathVariable int id, @Valid @RequestBody UserUpdateDto dto,
+		     BindingResult bindingResult){
+		//유효성
+		if (bindingResult.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			throw new MyAsyncNotFoundException(errorMap.toString());
+		}
+		
+		//인증
+			User principal = (User) session.getAttribute("principal");
+			if (principal == null) { // 로그인 안됨
+				throw new MyAsyncNotFoundException("인증이 되지 않았습니다.");
+			}
+		//권한
+			if (principal.getId() != id) {
+				throw new MyAsyncNotFoundException("회원정보를 수정할 권한이 없습니다.");
+			}
+		// 핵심로직	
+			principal.setEmail(dto.getEmail());
+			session.setAttribute("principal", principal); // 세션 값 변경
+
+			userRepository.save(principal);
+			
+			return new CMRespDto<>(1, "성공", null);
+		}
 	
 	@PostMapping("/login")
 	public String login(LoginReqDto dto) {
@@ -32,7 +74,7 @@ public class UserController {
 		if(principal == null) {
 			return "redirect:/loginForm";
 		}else {
-			Session.setAttribute("principal", principal);
+			session.setAttribute("principal", principal);
 			return "redirect:/";
 		}
 		
@@ -67,9 +109,10 @@ public class UserController {
 	
 	@GetMapping("/logout")
 	public String logout() {
-		Session.invalidate();
+		session.invalidate();
 		return "redirect:/";
 		
 	}
-	
 }
+
+
